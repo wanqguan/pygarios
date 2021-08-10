af4c1e800e6a521bdf9a03be59b3ec65b05751d3
shiyunlai233 <shiyunlaichina@163.com>
2021-08-10 23:46:31 +0800

    Update demo0_hello_world.py

--- demo0_hello_world.py
+++ demo0_hello_world.py
@@ -1,18 +1,19 @@
 from pygarios import *
 import pygame
 
+# 定义窗口的长度和宽度，单位是像素
 width, height = 900, 900
 
-
+# 处理Pygame事件（包括键盘鼠标的输入等），开头两行是必须加的
 def pygame_process_events(events):
     global mousePressed, mouseX, mouseY
     mousePressed, mouseX, mouseY = update_mouse_state_from_pygame_events(events)
 
-
+# 项目初始化时候，执行一次
 def start():
     background(1, 1, 1)
     
-
+# 项目初始化后，循环执行本函数
 def update():
     pygame_process_events(pygame.event.get())  # pyplay 特殊语法
 
@@ -37,5 +38,6 @@ def update():
         circle(mouseX, mouseY, 20)
 
 
-if __name__ == "__main__":  # pyplay 特殊语法
+# 固定写法
+if __name__ == "__main__":  
     main(start, update, width, height)
